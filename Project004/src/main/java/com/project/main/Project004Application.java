118cbe9bb959eb49978a77da069392c9f887fcb5
FruegaGit <fruedagit@gmail.com>
2022-06-20 16:31:20 +0200

    initialCommit

---
+++ Project004/src/main/java/com/project/main/Project004Application.java
@@ -0,0 +1,13 @@
+package com.project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project004Application.class, args);
+	}
+
+}
